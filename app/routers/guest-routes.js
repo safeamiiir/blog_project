0dded031d3cb8efb7078c0e6c57a56973ff0c0a5
safeamiiir <safehian_amirreza@yahoo.com>
2018-07-15 12:03:23 +0430

    Profile And Post EJS Linked to Other Pages Their Data in mined From DB and shown for each user left : 1. Comment 2. Statistics

--- app/routers/guest-routes.js
+++ app/routers/guest-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Article = require('../models/articles');
-const mongoose = require('mongoose');                    
+const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 router.get("/",function(req,res){
@@ -16,16 +16,40 @@ router.get("/",function(req,res){
     res.sendFile("/Users/amir/WebstormProjects/Blog_Project/view/index.html"); //mac
 });
 
-router.get("/post",function(req,res){
+router.get("/post/:postID",function(req,res){
     // res.render("E:/Ducuments/Makab/Blog_Project/BlogNode/view/postPage.ejs"); // Win,Masoud
     // res.render("C:/Users/Alireza/Desktop/Blog_Project/view/postPage.ejs"); // Win,Alireza
-    res.render("/Users/amir/WebstormProjects/Blog_Project/view/postPage.ejs");  // Mac
+    console.log(" Post ID :",req.params.postID);
+    Article.findOne({ _id : req.params.postID },function(err, art) {
+        console.log(" \n\n\n\n Heeeey ! Look At The Data : \n" , art , " \n\n\n\n");
+        res.render("/Users/amir/WebstormProjects/Blog_Project/view/postPage.ejs", {
+            art:art
+        });
+    });
 });
-
-router.get("/profile" , function(req , res){
+router.get("/profile", function (req,res) {
+    res.send( " مقاله ای جهت نمایش موجود نیست  ");
+});
+router.get("/profile/:userName" , function(req , res){
     // res.render("E:/Ducuments/Makab/Blog_Project/BlogNode/view/profile.ejs"); // Win,Masoud
     // res.render("C:/Users/Alireza/Desktop/Blog_Project/view/profile.ejs"); // Win,Alireza
-    res.render("/Users/amir/WebstormProjects/Blog_Project/view/profile.ejs"); // Mac
+
+    console.log("userName : ",req.params.userName);
+    Article.count({ author : req.params.userName },function(err, count) {
+        console.log("Number Of Articles : ",count);
+
+        Article.find({ author : req.params.userName }).sort("-createDate").exec(
+            function (err, art) {
+                console.log( "\n\n here \n\n",art,"\n\n here \n\n ");
+                res.render('/Users/amir/WebstormProjects/Blog_Project/view/profile.ejs', {
+                    art: art,
+                    userName: req.params.userName,
+                    artNum: count
+                })
+            }
+        );
+        // }
+    });
 });
 module.exports = router;
 
